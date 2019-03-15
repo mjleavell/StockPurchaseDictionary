8505032971a5bc83c3b7da5fcfea2f8df7641c44
Maggie Leavell <mjleavell@gmail.com>
2019-03-14 22:42:51 -0500

    added stock prices

--- StockPurchaseDictionary/StockPurchaseDictionary/Program.cs
+++ StockPurchaseDictionary/StockPurchaseDictionary/Program.cs
@@ -21,17 +21,26 @@ namespace StockPurchaseDictionary
 
         static void Main(string[] args)
         {
-            Dictionary<string, string> Stocks = new Dictionary<string, string>();
-            Stocks.Add("NKE", "Nike");
-            Stocks.Add("AAPL", "Apple");
-            Stocks.Add("GS", "Goldman Sachs");
-            Stocks.Add("TWTR", "Twitter");
-            Stocks.Add("SBUX", "Starbucks");
-            Stocks.Add("XOM", "Exxon");
-            Stocks.Add("SNAP", "Snapchat");
-            Stocks.Add("GOOGL", "Google");
-            Stocks.Add("TSLA", "Tesla");
+            Dictionary<string, string> stocks = new Dictionary<string, string>();
+            stocks.Add("NKE", "Nike");
+            stocks.Add("AAPL", "Apple");
+            stocks.Add("TWTR", "Twitter");
+            stocks.Add("SNAP", "Snapchat");
+            stocks.Add("GOOGL", "Google");
 
+            List<(string ticker, int shares, double price)> purchases = new List<(string, int, double)>();
+            purchases.Add((ticker: "NKE", shares: 50, price:86.87));
+            purchases.Add((ticker: "NKE", shares: 123, price: 86.87));
+            purchases.Add((ticker: "NKE", shares: 13, price: 86.87));
+            purchases.Add((ticker: "AAPL", shares: 414, price: 183.73));
+            purchases.Add((ticker: "AAPL", shares: 63, price: 183.73));
+            purchases.Add((ticker: "AAPL", shares: 27, price: 183.73));
+            purchases.Add((ticker: "TWTR", shares: 223, price: 31.03));
+            purchases.Add((ticker: "TWTR", shares: 77, price: 31.03));
+            purchases.Add((ticker: "SNAP", shares: 90, price: 11.28));
+            purchases.Add((ticker: "SNAP", shares: 303, price: 11.28));
+            purchases.Add((ticker: "GOOGL", shares: 19, price: 1192.53));
+            purchases.Add((ticker: "GOOGL", shares: 36, price: 1192.53));
         }
     }
 }
